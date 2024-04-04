08a33620cb7c465266d64bfb310744549a718074
anlaw62 <anlaw62@mail.ru>
2024-04-04 13:44:20 +0300

    узел перемещения

---
+++ Runtime/Nodes/MovementNode.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace MBT
+{
+    public class MovementNode : Leaf
+    {
+        [SerializeField] private Vector3Variable position;
+        public override void OnEnter()
+        {
+            navMeshAgent.SetDestination(position.Value);
+        }
+        public override NodeResult Execute()
+        {
+            if (!navMeshAgent.isStopped) return NodeResult.running;
+            else if (
+                navMeshAgent.pathStatus == NavMeshPathStatus.PathInvalid
+                ||
+                navMeshAgent.pathStatus == NavMeshPathStatus.PathPartial) return NodeResult.failure;
+            return NodeResult.success;
+        }
+    }
+}
